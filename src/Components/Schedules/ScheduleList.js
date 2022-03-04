f54d4672c3145ed418c1695d8bd4d62c902de9f7
Julie Fitzpatrick <juliemfitzpatrick@gmail.com>
2022-03-04 16:24:09 -0330

    adds schedule item component

--- src/Components/Schedules/ScheduleList.js
+++ src/Components/Schedules/ScheduleList.js
@@ -2,44 +2,49 @@ import React, { useState } from "react";
 import Box from "@mui/material/Box";
 import Grid from "@mui/material/Grid";
 import Typography from "@mui/material/Typography";
+import ScheduleItem from "./ScheduleItem";
 
 export default function ScheduleList() {
 
     const [schedules, setSchedules] = useState([]);
 
     return (
-            <Box sx={{ flexGrow: 1, minHeight: "100vh" }}>
-                <Grid container spacing={2} columns={16}>
-                    <Grid item xs={8}
-                        sx={{
-                            transition: "300ms ease",
-                            "&:hover > .MuiGrid-item": {
-                                opacity: 0.5,
-                            },
+        <Box sx={{ flexGrow: 1, minHeight: "100vh" }}>
+            <Grid container spacing={2} columns={16}>
+                <Grid item xs={8}
+                    sx={{
+                        transition: "300ms ease",
+                        "&:hover > .MuiGrid-item": {
+                            opacity: 0.5,
+                        },
 
-                            ".MuiGrid-item:hover": {
-                                opacity: 1,
-                            },
-                            ".MuiGrid-item:hover .MuiTypography-root": {
-                                color: "white !important",
-                            },
-                        }}>
-                        <>
-                            <Grid container direction="row">
-                                <Grid item xs={13.7}>
-                                    <Typography variant='h4' component='h2'>
-                                        Your schedule items
-                                    </Typography>
-                                </Grid>
-                                <Grid item xs={2}>
-                                </Grid>
+                        ".MuiGrid-item:hover": {
+                            opacity: 1,
+                        },
+                        ".MuiGrid-item:hover .MuiTypography-root": {
+                            color: "grey !important",
+                        },
+                    }}>
+                    <>
+                        <Grid container direction="row">
+                            <Grid item xs={13.7}>
+                                <Typography variant='h4' component='h2'>
+                                    Your schedule items
+                                    <ScheduleItem
+                                        key={Math.random().toString(36).substr(2, 9)}
+                                    />
+                                </Typography>
                             </Grid>
-                        </>
-                    </Grid>
-                    <Grid item xs={8}>
-                        Hello there!
-                    </Grid>
+                            <Grid item xs={2}>
+                                This is an item
+                            </Grid>
+                        </Grid>
+                    </>
                 </Grid>
-            </Box>
+                <Grid item xs={8}>
+                    Hello there!
+                </Grid>
+            </Grid>
+        </Box>
     );
 }
