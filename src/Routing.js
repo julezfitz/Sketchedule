40f273b70fb345f7b5137088e4377efa7302423c
Julie Fitzpatrick <juliemfitzpatrick@gmail.com>
2022-03-04 15:25:45 -0330

    set up react routing files

---
+++ src/Routing.js
@@ -0,0 +1,57 @@
+import React, { useEffect, useState } from "react";
+import { Routes, Route } from "react-router-dom";
+import Heading from "./Heading/Heading";
+import { Divider } from "@mui/material";
+
+
+export default function Routing({ location }) {
+
+    const [headingTitle, setHeadingTitle] = useState(null);
+
+    const pageInfo = [
+        {
+            headingTitle: "Home",
+            path: "/",
+        },
+        {
+            headingTitle: "My Schedules",
+            path: "/offers",
+        },
+    ];
+
+    useEffect(() => {
+        for (let page of pageInfo) {
+            if (page.path === location.pathname) {
+                setHeadingTitle(page.headingTitle);
+            }
+        }
+    }, [location]);
+
+    return (
+        <>
+            <Heading
+                size="medium"
+                className="page-heading"
+                color="light"
+                style={{
+                    marginBottom: 0,
+                    marginTop: "2rem",
+
+                    overflow: "hidden",
+                }}
+            >
+                {headingTitle}
+            </Heading>
+            <Divider
+                sx={{ marginBottom: "5rem", marginTop: "1rem" }}
+                color="white"
+            />
+            <Routes location={location} key={location.pathname}>
+                <Route
+                    path="/"
+                />
+            </Routes>
+        </>
+    )
+}
+
