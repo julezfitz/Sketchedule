fe5c20959c088559173bfb4c03710e0b8fa007fb
Julie Fitzpatrick <juliemfitzpatrick@gmail.com>
2022-03-07 16:08:17 -0330

    converts schedule view to use image list

--- src/Components/Schedules/EditSchedule.js
+++ src/Components/Schedules/EditSchedule.js
@@ -3,6 +3,9 @@ import EditIcon from '@mui/icons-material/Edit';
 import {
   IconButton, Card, CardContent, CardActionArea, CardMedia, Typography, Grid, Box, Paper,
 } from '@mui/material';
+import ImageList from '@mui/material/ImageList';
+import ImageListItem from '@mui/material/ImageListItem';
+import ImageListItemBar from '@mui/material/ImageListItemBar';
 import AddIcon from '@mui/icons-material/Add';
 
 export default function EditSchedule() {
@@ -18,7 +21,7 @@ export default function EditSchedule() {
           <EditIcon />
         </IconButton>
       </Typography>
-      <Grid container spacing={2} columns={16}>
+      {/* <Grid container spacing={2} columns={16}>
         <Grid
           item
           xs={16}
@@ -35,93 +38,64 @@ export default function EditSchedule() {
               color: 'grey !important',
             },
           }}
+        > */}
+      <ImageList rowHeight={134} cols={2} gap={8} sx={{ width: '100%', height: 450 }}>
+
+        {/* <Grid container direction="row" rows={2}> */}
+        <ImageListItem key={Math.random().toString(36).substr(2, 9)}>
+          <Card
+            sx={{ paddingLeft: '20%' }}
+            style={{ minHeight: '22.5vh', justifyContent: 'center', display: 'flex' }}
+            variant="outlined"
+            alignItems="center"
+          >
+            <CardActionArea>
+              <AddIcon
+                sx={{ fontSize: 100, color: 'gray' }}
+              />
+            </CardActionArea>
+          </Card>
+        </ImageListItem>
+
+        <ImageListItem
+          key={Math.random().toString(36).substr(2, 9)}
         >
-          <Paper style={{ maxHeight: 450, overflow: 'auto' }}>
-
-            <Grid container direction="row">
-
-              <Grid container direction="column">
-
-                <Grid item xs={18}>
-                  <Card
-                    sx={{ width: '10em' }}
-                    style={{ minHeight: '25vh' }}
-                    variant="outlined"
-                    direction="column"
-                  >
-                    <CardActionArea>
-                      <AddIcon
-                        sx={{ fontSize: 120, color: 'gray' }}
-                      />
-                      <CardContent
-                        sx={{
-                          paddingTop: 1,
-                          paddingBottom: 1,
-                        }}
-                      />
-                    </CardActionArea>
-                  </Card>
-                </Grid>
-                {/* loop here to create cards */}
-                <Grid item xs={18}>
-                  <Card
-                    sx={{ width: '10em' }}
-                    style={{ minHeight: '25vh' }}
-                    variant="outlined"
-                    direction="column"
-                  >
-                    <CardActionArea>
-                      <CardMedia
-                        component="img"
-                        height="140"
-                        image="https://images.unsplash.com/photo-1525507119028-ed4c629a60a3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"
-                        alt="clothing"
-                      />
-                      <CardContent
-                        sx={{
-                          paddingTop: 1,
-                          paddingBottom: 1,
-                        }}
-                      >
-                        <Typography gutterBottom component="div">
-                          Get Dressed
-                        </Typography>
-                      </CardContent>
-                    </CardActionArea>
-                  </Card>
-                </Grid>
-                <Grid item xs={18}>
-                  <Card
-                    sx={{ width: '10em' }}
-                    style={{ minHeight: '25vh' }}
-                    variant="outlined"
-                    direction="column"
-                  >
-                    <CardActionArea>
-                      <CardMedia
-                        component="img"
-                        height="140"
-                        image="https://images.unsplash.com/photo-1575783970733-1aaedde1db74?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1752&q=80"
-                        alt="clothing"
-                      />
-                      <CardContent
-                        sx={{
-                          paddingTop: 1,
-                          paddingBottom: 1,
-                        }}
-                      >
-                        <Typography gutterBottom component="div">
-                          Go to Playground
-                        </Typography>
-                      </CardContent>
-                    </CardActionArea>
-                  </Card>
-                </Grid>
-              </Grid>
-            </Grid>
-          </Paper>
-        </Grid>
-      </Grid>
+          <img
+            src="https://images.unsplash.com/photo-1525507119028-ed4c629a60a3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"
+            alt="Clothing"
+            style={{ height: 150 }}
+            loading="lazy"
+          />
+          <ImageListItemBar
+            title="Get Dressed"
+            position="below"
+          />
+        </ImageListItem>
+        <ImageListItem key={Math.random().toString(36).substr(2, 9)}>
+          <img
+            src="https://images.unsplash.com/photo-1575783970733-1aaedde1db74?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1752&q=80"
+            alt="Playground"
+            style={{ height: 150 }}
+            loading="lazy"
+          />
+          <ImageListItemBar
+            title="Go to Playground"
+            position="below"
+          />
+        </ImageListItem>
+        <ImageListItem key={Math.random().toString(36).substr(2, 9)}>
+          <img
+            src="https://images.unsplash.com/photo-1475090169767-40ed8d18f67d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1932&q=80"
+            alt="Lunch"
+            style={{ height: 150 }}
+            loading="lazy"
+          />
+          <ImageListItemBar
+            title="Get Lunch"
+            position="below"
+          />
+        </ImageListItem>
+      </ImageList>
     </Box>
   );
 }
