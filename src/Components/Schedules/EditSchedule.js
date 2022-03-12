587bdb8b89c7afe65bf87276119a74ee0b2c49c6
Julie Fitzpatrick <juliemfitzpatrick@gmail.com>
2022-03-12 20:14:55 -0330

    resizes image list to be consistent

--- src/Components/Schedules/EditSchedule.js
+++ src/Components/Schedules/EditSchedule.js
@@ -7,10 +7,11 @@ import ImageList from '@mui/material/ImageList';
 import ImageListItem from '@mui/material/ImageListItem';
 import ImageListItemBar from '@mui/material/ImageListItemBar';
 import AddIcon from '@mui/icons-material/Add';
+import { NoEncryption } from '@mui/icons-material';
 
 export default function EditSchedule() {
   return (
-    <Box sx={{ flexGrow: 1, minHeight: '60vh' }}>
+    <Box>
       <Typography>
         New Sketchedule - July 10, 2022
         <IconButton
@@ -21,17 +22,28 @@ export default function EditSchedule() {
           <EditIcon />
         </IconButton>
       </Typography>
-      <ImageList rowHeight={100} cols={2} gap={8} sx={{ height: 450 }}>
+      <ImageList
+        sx={{
+          img: {
+            minHeight: '80%',
+          },
+          div: {
+            minHeight: '20%',
+          },
+        }}
+        cols={2}
+        rowHeight={150}
+        gap={8}
+      >
         <ImageListItem key={Math.random().toString(36).substr(2, 9)}>
           <Card
             sx={{ paddingLeft: '20%' }}
-            style={{ minHeight: '22.5vh', justifyContent: 'center', display: 'flex' }}
+            style={{ minHeight: '80%', justifyContent: 'center', display: 'flex' }}
             variant="outlined"
-            alignItems="center"
           >
             <CardActionArea>
               <AddIcon
-                sx={{ fontSize: 100, color: 'gray' }}
+                sx={{ fontSize: 100, color: 'gray', justifyContent: 'center' }}
               />
             </CardActionArea>
           </Card>
@@ -39,9 +51,11 @@ export default function EditSchedule() {
 
         <ImageListItem key={Math.random().toString(36).substr(2, 9)}>
           <img
-            src="https://images.unsplash.com/photo-1525507119028-ed4c629a60a3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"
+            // src="https://images.unsplash.com/photo-1525507119028-ed4c629a60a3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"
             alt="Clothing"
-            style={{ height: 150 }}
+            // style={{ height: 150 }}
+            src="https://images.unsplash.com/photo-1525507119028-ed4c629a60a3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8"
+            // srcSet={`${item.img}?w=164&h=164&fit=crop&auto=format&dpr=2 2x`}
             loading="lazy"
           />
           <ImageListItemBar
@@ -51,9 +65,9 @@ export default function EditSchedule() {
         </ImageListItem>
         <ImageListItem key={Math.random().toString(36).substr(2, 9)}>
           <img
-            src="https://images.unsplash.com/photo-1575783970733-1aaedde1db74?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1752&q=80"
+            src="https://images.unsplash.com/photo-1575783970733-1aaedde1db74?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8"
             alt="Playground"
-            style={{ height: 150 }}
+            // style={{ height: 150 }}
             loading="lazy"
           />
           <ImageListItemBar
@@ -63,13 +77,39 @@ export default function EditSchedule() {
         </ImageListItem>
         <ImageListItem key={Math.random().toString(36).substr(2, 9)}>
           <img
-            src="https://images.unsplash.com/photo-1475090169767-40ed8d18f67d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1932&q=80"
+            src="https://images.unsplash.com/photo-1475090169767-40ed8d18f67d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8"
             alt="Lunch"
-            style={{ height: 150 }}
+            // style={{ height: 150 }}
             loading="lazy"
           />
           <ImageListItemBar
             title="Get Lunch"
+            position="below"
+          />
+        </ImageListItem>
+        <ImageListItem key={Math.random().toString(36).substr(2, 9)}>
+          <img
+            src="https://images.unsplash.com/photo-1575783970733-1aaedde1db74?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8"
+            alt="Playground"
+            // style={{ height: 150 }}
+            loading="lazy"
+          />
+          <ImageListItemBar
+            title="Go to Playground"
+            position="below"
+          />
+        </ImageListItem>
+        <ImageListItem key={Math.random().toString(36).substr(2, 9)}>
+          <img
+            // src="https://images.unsplash.com/photo-1525507119028-ed4c629a60a3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"
+            alt="Clothing"
+            // style={{ height: 150 }}
+            src="https://images.unsplash.com/photo-1525507119028-ed4c629a60a3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8"
+            // srcSet={`${item.img}?w=164&h=164&fit=crop&auto=format&dpr=2 2x`}
+            loading="lazy"
+          />
+          <ImageListItemBar
+            title="Get Dressed"
             position="below"
           />
         </ImageListItem>
