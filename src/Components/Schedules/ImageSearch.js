53495ee4d189abea5cc2af80c4b3b32d9bbb5baf
Julie Fitzpatrick <juliemfitzpatrick@gmail.com>
2022-03-17 16:28:38 -0230

    handles search by making api call - still errors in call itself

--- src/Components/Schedules/ImageSearch.js
+++ src/Components/Schedules/ImageSearch.js
@@ -1,10 +1,9 @@
 import React, { useState } from 'react';
 import { createApi } from 'unsplash-js';
 import {
-  TextField, Box, Grid,
+  TextField, Box, Grid, Button,
 } from '@mui/material';
 import InputAdornment from '@mui/material/InputAdornment';
-import FormControl from '@mui/material/FormControl';
 import SearchIcon from '@mui/icons-material/Search';
 
 export default function ImageSearch() {
@@ -16,7 +15,11 @@ export default function ImageSearch() {
   });
 
   const handleSearch = () => {
-    unsplash.search.photos('cats', 1)
+    unsplash.search.getPhotos({
+      query: 'cat',
+      page: 1,
+      perPage: 10,
+    })
       .then((response) => {
         console.log(response);
       });
@@ -24,23 +27,27 @@ export default function ImageSearch() {
 
   return (
     <Box sx={{ flexGrow: 1, minHeight: '60vh' }}>
-      <FormControl variant="standard" onSubmit={() => handleSearch()}>
-        <TextField
-          id="outlined-basic"
-          label="Search"
-          placeholder="ex. Playground"
-          value={searchTerm}
-          onChange={(e) => setSearchTerm(e.target.value)}
-          InputProps={{
-            startAdornment: (
-              <InputAdornment position="start">
-                <SearchIcon />
-              </InputAdornment>
-            ),
-          }}
-          variant="outlined"
-        />
-      </FormControl>
+      <TextField
+        id="outlined-basic"
+        label="Search"
+        placeholder="ex. Playground"
+        value={searchTerm}
+        onChange={(e) => setSearchTerm(e.target.value)}
+        InputProps={{
+          startAdornment: (
+            <InputAdornment position="start">
+              <SearchIcon />
+            </InputAdornment>
+          ),
+        }}
+        variant="outlined"
+      />
+      <Button
+        variant="success"
+        onClick={() => handleSearch()}
+      >
+        Search
+      </Button>
       <Grid container spacing={2} columns={16}>
         <Grid
           item
