97c36945e2f718f866303182c19443918474a673
Julie Fitzpatrick <juliemfitzpatrick@gmail.com>
2022-03-20 13:06:08 -0230

    creates db file and adds sample data structure

---
+++ src/db.js
@@ -0,0 +1,19 @@
+let db = {};
+
+// data structure
+// {
+//     'schedules': [
+//         {
+//             'scheduleName': 'name',
+//             'dateCreated':'date',
+//             'scheduleItems': [
+//                 {
+//                     'imageSrc': 'url or ref',
+//                     'altText': 'description',
+//                     'imageLabel': 'label',
+//                     'complete': 'boolean'
+//                 },
+//             ]
+//         },
+//     ],
+// }
