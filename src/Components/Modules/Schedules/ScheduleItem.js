79a84a56765453a500758686a3edaace237935f1
Julie Fitzpatrick <juliemfitzpatrick@gmail.com>
2022-04-16 08:42:35 -0230

    adds a drop down menu for schedule options

--- src/Components/Modules/Schedules/ScheduleItem.js
+++ src/Components/Modules/Schedules/ScheduleItem.js
@@ -1,17 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
-  IconButton, Typography, Paper, Grid,
+  IconButton, Typography, Paper, Grid, MenuList, MenuItem, Menu, ListItemText, ListItemIcon,
 } from '@mui/material';
 import { Edit, Delete } from '@mui/icons-material';
+import MoreHorizIcon from '@mui/icons-material/MoreHoriz';
 
 export default function ScheduleItem({ name }) {
+  const [anchorEl, setAnchorEl] = useState(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
   return (
     <Paper variant="outlined" sx={{ p: 2, m: 1, flexGrow: 1 }} style={{ cursor: 'pointer' }}>
       <Grid container spacing={1}>
         <Grid item xs={16} sm container>
-          <Grid item xs container direction="column" spacing={8}>
+          <Grid item xs={13} container direction="column" spacing={1}>
             <Grid item xs>
-              <Typography gutterBottom variant="subtitle1" component="div">
+              <Typography gutterBottom variant="h6" component="div">
                 {name}
               </Typography>
             </Grid>
@@ -21,18 +31,48 @@ export default function ScheduleItem({ name }) {
             <IconButton
               size="large"
               color="inherit"
-              style={{ marginLeft: 2.5 }}
+              style={{ marginLeft: '10%' }}
+              id="options-button"
+              aria-controls={open ? 'options-menu' : undefined}
+              aria-haspopup="true"
+              aria-expanded={open ? 'true' : undefined}
+              variant="contained"
+              disableElevation
+              onClick={handleClick}
             >
-              <Edit />
+              <MoreHorizIcon />
             </IconButton>
 
-            <IconButton
-              size="large"
-              color="inherit"
-              style={{ marginLeft: 0 }}
+            <Menu
+              id="options-menu"
+              MenuListProps={{
+                'aria-labelledby': 'options-button',
+              }}
+              anchorEl={anchorEl}
+              open={open}
+              onClose={handleClose}
             >
-              <Delete />
-            </IconButton>
+              <MenuList>
+                <MenuItem>
+                  <ListItemIcon
+                    size="large"
+                    color="inherit"
+                  >
+                    <Edit />
+                  </ListItemIcon>
+                  <ListItemText>Edit</ListItemText>
+                </MenuItem>
+                <MenuItem>
+                  <ListItemIcon
+                    size="large"
+                    color="inherit"
+                  >
+                    <Delete />
+                  </ListItemIcon>
+                  <ListItemText>Delete</ListItemText>
+                </MenuItem>
+              </MenuList>
+            </Menu>
           </Grid>
 
         </Grid>
