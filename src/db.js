75e1d0dd2832323e1ccd9a544e730db1758b3fd4
Julie Fitzpatrick <juliemfitzpatrick@gmail.com>
2022-05-29 09:45:41 -0230

    add schedule ID to schedule items

--- src/db.js
+++ src/db.js
@@ -2,7 +2,7 @@ import Dexie from 'dexie';
 
 export const db = new Dexie('sketcheduleDatabase');
 db.version(1).stores({
-  scheduleItems: '++id, imageSrc, altText, imageLabel, complete',
+  scheduleItems: '++id, imageSrc, altText, imageLabel, complete, scheduleID',
   schedules: '++id, name, dateCreated',
 });
 
