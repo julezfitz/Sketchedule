3a40474cf15c8d5e7faa8e595bbe9d963c261731
Julie Fitzpatrick <juliemfitzpatrick@gmail.com>
2022-06-05 09:57:47 -0230

    removes fake items from edit view

--- src/Components/Modules/Schedules/EditSchedule.js
+++ src/Components/Modules/Schedules/EditSchedule.js
@@ -142,69 +142,6 @@ export default function EditSchedule() {
             />
           </ImageListItem>
         ))}
-        <ImageListItem key="11">
-          <img
-            alt="Clothing"
-            src="https://images.unsplash.com/photo-1525507119028-ed4c629a60a3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8"
-            loading="lazy"
-          />
-          <IconButton
-            size="medium"
-            style={{ color: 'white', marginLeft: 130, position: 'absolute' }}
-            onClick={() => handleDeleteItem()}
-          >
-            <Delete />
-          </IconButton>
-
-          <ImageListItemBar
-            title="Get Dressed"
-            position="below"
-          />
-        </ImageListItem>
-        <ImageListItem key="12">
-          <img
-            src="https://images.unsplash.com/photo-1575783970733-1aaedde1db74?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8"
-            alt="Playground"
-            loading="lazy"
-          />
-          <ImageListItemBar
-            title="Go to Playground"
-            position="below"
-          />
-        </ImageListItem>
-        <ImageListItem key="13">
-          <img
-            src="https://images.unsplash.com/photo-1475090169767-40ed8d18f67d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8"
-            alt="Lunch"
-            loading="lazy"
-          />
-          <ImageListItemBar
-            title="Get Lunch"
-            position="below"
-          />
-        </ImageListItem>
-        <ImageListItem key="14">
-          <img
-            src="https://images.unsplash.com/photo-1575783970733-1aaedde1db74?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8"
-            alt="Playground"
-            loading="lazy"
-          />
-          <ImageListItemBar
-            title="Go to Playground"
-            position="below"
-          />
-        </ImageListItem>
-        <ImageListItem key="15">
-          <img
-            alt="Clothing"
-            src="https://images.unsplash.com/photo-1525507119028-ed4c629a60a3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8"
-            loading="lazy"
-          />
-          <ImageListItemBar
-            title="Get Dressed"
-            position="below"
-          />
-        </ImageListItem>
       </ImageList>
     </Box>
   );
