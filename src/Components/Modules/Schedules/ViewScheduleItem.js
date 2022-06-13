2e1ba2136b36b9a4d7036c10f2796f5001ffd6c2
Julie Fitzpatrick <juliemfitzpatrick@gmail.com>
2022-06-12 21:32:13 -0230

    break out view only list items into own component

---
+++ src/Components/Modules/Schedules/ViewScheduleItem.js
@@ -0,0 +1,44 @@
+import React, { useRef, useState, useEffect } from 'react';
+import { KeyboardDoubleArrowDown } from '@mui/icons-material';
+import { Stack, Checkbox } from '@mui/material';
+import ImageListItem from '@mui/material/ImageListItem';
+import ImageListItemBar from '@mui/material/ImageListItemBar';
+
+export default function ViewScheduleItem(props) {
+  const { item, scheduleItems, handleItemCompleteChange } = props;
+  const [complete, setComplete] = useState(item.complete);
+
+  return (
+    <Stack key={item.id + 50} display="flex" alignItems="center" justify="center">
+      <ImageListItem>
+        <img
+          alt={item.altText}
+          src={item.imageSrc}
+          loading="lazy"
+        />
+        <ImageListItemBar
+          title={item.imageLabel}
+          actionIcon={(
+            <Checkbox
+              size="medium"
+              checked={complete === 'true'}
+              onChange={(event) => {
+                handleItemCompleteChange(event, item.id);
+                setComplete(event.target.checked);
+              }}
+              sx={{
+                color: 'rgba(255, 255, 255, 0.54)',
+                '&.Mui-checked': {
+                  color: 'rgba(255, 255, 255, 0.54)',
+                },
+              }}
+              aria-label="mark as complete"
+            />
+                    )}
+        />
+      </ImageListItem>
+      {item !== scheduleItems[scheduleItems.length - 1]
+            && (<KeyboardDoubleArrowDown sx={{ marginTop: 1 }} />)}
+    </Stack>
+  );
+}
